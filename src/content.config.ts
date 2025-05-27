c2011eba972c183d38e8d56da077669753fbf600
Jordan Ayotte <jordanayotte13@outlook.com>
2025-05-27 12:01:25 -0400

    rendering blog post in index page

---
+++ src/content.config.ts
@@ -0,0 +1,32 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const blog = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+    })
+});
+const projects = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/data/projects" }),
+    schema: z.object({
+        title: z.string(),
+        image: z.object({
+            src: z.string(),
+            alt: z.string(),
+        }),
+        description: z.string(),
+        pubDate: z.coerce.date(),
+        updatedDate: z.coerce.date().optional(),
+    })
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { blog, projects };
---
+++ src/data/blog/test.md
@@ -0,0 +1,8 @@
+---
+title: blog post
+description: test description
+pubDate: 2025-05-27
+
+---
+
+# Hello
