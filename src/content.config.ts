462925280a97e9f635545bd946c31b5bf568aa1d
Jordan Ayotte <jordanayotte13@outlook.com>
2026-07-29 13:03:27 -0400

    using larger images on desktop view chefkit

--- src/content.config.ts
+++ src/content.config.ts
@@ -19,7 +19,7 @@ const projects = defineCollection({
     schema: z.object({
         title: z.string(),
         demoURL: z.string().optional(),
-        repoURL: z.string(),
+        repoURL: z.string().optional(),
         description: z.string(),
         pubDate: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
